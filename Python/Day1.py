5ef1fa5901d8d1e107177eb40e5e6b52f1867659
SathvikSesha <lakshmisarmatk@gmail.com>
2025-12-14 10:08:56 +0530

    Python basics

---
+++ Python/Day1.py
@@ -0,0 +1,45 @@
+# #Question - 1 
+# name = input("Enter you name: ")
+# age = int(input("Enter the your age: "))
+# print("Hi ",name," your age is ",age)
+
+# #Question - 2
+# a = int(input("Enter the number-1: "))
+# b = int(input("Enter the number-2: "))
+# print("Sum:", a + b)
+# print("Difference:", a - b if a > b else b - a)
+# print("Product:", a * b)
+# print("Quotient:", a / b if a > b else b / a)
+
+# #Question - 3
+# num1 = int(input("Enter the number-1: "))
+# num2 = int(input("Enter the number-2: "))
+# sum = float(num1+num2)
+# print("The average of 2 numbers is: ",sum/2)
+
+# #Question - 4
+# str1 = input("Enter some number: ")
+# print("The entered number in Integer:",int(str1))
+# print("The entered number in Float:",float(str1))
+# print("The entered number in String:",str(str1))
+
+# #Question - 5
+# x = 10+3*2**2
+# print(x)
+
+# #Question - 6
+# a = int(input("enter the 1st number:"))
+# b = int(input("enter the 2nd number:"))
+# a,b = b,a
+# print(f"numbers after the swap is number-1: {a} , number-2: {b}")
+
+# #Question - 7
+# tempc = float(input("enter the temperature in celsius:"))
+# tempf = tempc*(9/5) + 32
+# print(f"The temperature in celsius {tempc} - Fahrenheit {tempf}")
+
+# #Question - 8
+# from math import pi
+# radius = float(input("Enter the radius:"))
+# area = 2*pi*radius
+# print(f"The area of the circle is {area:.3f}")

0332993ce3e917a86fbda0b20cd564ef3f28406f
SathvikSesha <lakshmisarmatk@gmail.com>
2025-12-15 11:03:15 +0530

    Python Wraps

---
+++ Python/Day6.py
@@ -0,0 +1,30 @@
+# with open("D:/AIML/AIML/Python/names.txt", "w") as f:
+#     for i in range(5):
+#         name = input(f"Enter name {i + 1}: ")
+#         f.write(name + "\n")
+# with open("D:/AIML/AIML/Python/names.txt", "r") as f:
+#     print("\nNames in file:")
+#     for line in f:
+#         print(line.strip())
+
+
+# with open("log.txt","a") as f:
+#     f.write("Program run Sucessfully!!")
+
+# with open("log.txt","r") as f:
+#     print(f.read())
+
+# import json
+# dict = {
+#     "Guntur" : "10 Lakhs",
+#     "Vijayawada" : "15 Lakhs",
+#     "Vizag" : "20 lakhs"
+# }
+
+# with open("cities.json","w") as f:
+#     json.dump(dict,f)
+#     print(type(dict))
+
+# with open("cities.json",'r') as f:
+#     print(f.read())
+#     print(type(f.read()))
